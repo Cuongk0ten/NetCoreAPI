fec672fa627184f5c697f419b6b1fef008b50728
Cuongk0ten <cuongchip1210@gmail.com>
2024-05-15 15:53:47 +0700

    HTPP

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fad679abf6a602ed766473ebaf751c5c86d7a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54341e20bceb03d7775b32dc1324faa2ca67aa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
